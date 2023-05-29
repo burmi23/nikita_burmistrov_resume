4648f518d4a5430b3ef8470f4f3dd8323cd5ba39
Nikita Burmistrov <dev.burmi@gmail.com>
2023-05-29 20:56:32 +0300

    новые правки

--- js/hidden.js
+++ js/hidden.js
@@ -1,7 +1,10 @@
-const btnMore = document.querySelector('.articles-more');
-const articlesItems = document.querySelectorAll('.articles__item');
+const btnMore = document.querySelector('.btn-more');
+const cardsMore = document.querySelectorAll('.cards-item');
 
 btnMore.addEventListener('click', () => {
-    articlesItems.forEach(el => { el.classList.add('articles-item--visible') });
-    btnMore.closest('.articles-center').classList.add('articles-center--hidden');
+  cardsMore.forEach(el => { el.classList.toggle('cards-item--active') });
+  btnMore.closest('.btn-more-wrap').classList.add('btn--hidden');
+  /*в данном случае мы убираем класс btn-wrap из нащей разметки и добавляем класс btn--hidden чтобы скрыть кнопку */
+
+  // btnMore.classList.add('btn--hidden'); /*в данном случае мы добавляем класс btn--hidden чтобы скрыть кнопку , однако btn-wrap все равно остается в разметке */
 });
